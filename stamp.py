d4b655d2f89872e927c6b1405ec6fd64df0a0c41
Matthias Valvekens <matthias@mvalvekens.be>
2020-08-02 21:57:55 +0200

    Improve text rendering

--- stamp.py
+++ stamp.py
@@ -34,24 +34,33 @@ class FPDFImage(qrcode.image.base.BaseImage):
         stream.set_fill_color(0,0,0)
         for row, col in self._img:
             (x,y), _ = self.pixel_box(row, col)
-            stream.set_xy(
-                base_x + x, base_y + y
+            stream.rect(
+                base_x + x, base_y + y, self.box_size, self.box_size, style='F'
             )
-            stream.cell(self.box_size, self.box_size, fill=True)
+
 
 
 @dataclass(frozen=True)
 class StampStyle:
     font_size: int = 10
     font_family: str = 'Courier'
-    # TODO fix bogus defaults
-    stamp_width: int = 340
-    stamp_innsep: int = 3
-    stamp_textsep: int = 3
+    innsep: int = 3
+    textsep: int = 10
+    max_text_width = 300
 
     stamp_qrsize: int = 100
 
-    timestamp_format = '%Y-%m-%d %H:%M:%S'
+    # TODO support local timezone output
+    timestamp_format = '%Y-%m-%d %H:%M:%S UTC'
+
+    url_placeholder = '@@@'
+    stamp_text = (
+        "Digital version available at\n"
+        "this url: @@@\n\n"
+        "Timestamp: %(ts)s"
+    )
+
+
 
 
 class Stamper:
@@ -59,7 +68,8 @@ class Stamper:
     def __init__(self, style=None):
         self.style: StampStyle = style or StampStyle()
     
-    def stamp(self, input_handle, dest_page, x, y, url, output_handle=None):
+    def stamp(self, input_handle, dest_page, x, y, url, text_params=None, 
+              output_handle=None):
         style = self.style
         input_pdf = PdfFileReader(input_handle)
         dest_page_data = input_pdf.getPage(dest_page)
@@ -71,38 +81,49 @@ class Stamper:
         overlay.add_page()
         overlay.set_font(style.font_family, size=style.font_size)
 
-        text_x = x + 2 * style.stamp_innsep + style.stamp_qrsize
-        overlay.set_xy(text_x, y + style.stamp_innsep + style.stamp_textsep)
-        # TODO support local timezone output, linkify
-        # TODO set width by measuring string widths
-        stamp_text = (
-            "Digital version available at\n"
-            "%(url)s\n\n"
-            "Timestamp: %(ts)s UTC"
-        ) % {
-            'url': url, 
-            'ts': datetime.utcnow().strftime(style.timestamp_format)
-        }
-        overlay.multi_cell(
-            w=style.stamp_width - style.stamp_qrsize - 3 * style.stamp_innsep,
-            h=style.font_size + 2,
-            txt=stamp_text
-        )
+        # inner text area geometry
+        total_text_sep = style.innsep + style.textsep
+        until_text_start = 2 * style.innsep + style.stamp_qrsize
+        text_min_x = x + until_text_start
+        # line count * font size -> text height
+        # (TODO is this reliable with non-monospaced typefaces?)
+        text_height = len(style.stamp_text.split('\n')) * style.font_size
 
         # height is determined by the height of the text,
         # or the QR code, whichever is greater
-        stamp_height = max(
-            overlay.get_y() + style.stamp_innsep - y,
-            style.stamp_qrsize + 2 * style.stamp_innsep
-        )
-        overlay.set_xy(x, y)
-        overlay.cell(style.stamp_width, stamp_height, border=1)
+        # The width depends on the width of the inner text box
+        #  => compute later
 
+        stamp_height = max(
+            text_height + 2 * total_text_sep,
+            style.stamp_qrsize + 2 * style.innsep
+        )
+        text_y_sep = (stamp_height - text_height) // 2
+
+        # text rendering
+        l_margin_old, r_margin_old = overlay.l_margin, overlay.r_margin
+        overlay.set_left_margin(text_min_x)
+        overlay.set_right_margin(text_min_x + style.max_text_width)
+        overlay.set_xy(text_min_x, y + text_y_sep)
+        max_line_len = self._render_text(overlay, url, text_params)
+
+        # restore margins
+        overlay.set_left_margin(l_margin_old)
+        overlay.set_right_margin(r_margin_old)
+
+        stamp_width = (
+            until_text_start 
+            + min(style.max_text_width, max_line_len)
+            + total_text_sep
+        )
+
+        # draw border around stamp
+        overlay.rect(x, y, stamp_width, stamp_height)
 
         qr_y_sep = (stamp_height - style.stamp_qrsize) // 2
         overlay.set_xy(
-            x + style.stamp_innsep,
-            y + max(style.stamp_innsep, qr_y_sep)
+            x + style.innsep,
+            y + max(style.innsep, qr_y_sep)
         )
         self._draw_qr(overlay, url)
 
@@ -122,6 +143,47 @@ class Stamper:
         output_handle = output_handle or BytesIO()
         result.write(output_handle)
         return output_handle
+
+    def _render_text(self, overlay, url, text_params=None):
+        style = self.style
+        line_height = style.font_size
+
+        # render text
+        max_line_len = 0
+        _text_params = {
+            'ts': datetime.utcnow().strftime(style.timestamp_format),
+        }
+        if text_params is not None:
+            _text_params.update(text_params)
+        text = style.stamp_text % _text_params
+
+        # TODO Auto word-wrap is probably too much trouble, but
+        #  perhaps it's worth experimenting a little
+        for line in text.split('\n'):
+            line_len = 0
+            # replace any URL placeholders with links
+            # in most sane cases, the URL placeholder will only occur once,
+            # but why not do it robustly, eh
+            parts = iter(line.split('@@@'))
+
+            # first part always makes sense
+            part = next(parts) 
+            w = overlay.get_string_width(part)
+            line_len += w
+            overlay.cell(w, h=line_height, txt=part)
+            # handle any remaining parts, and insert links
+            # where appropriate
+            for part in parts:
+                w = overlay.get_string_width(url)
+                overlay.cell(w, h=line_height, txt=url, link=url)
+                line_len += w
+                w = overlay.get_string_width(part)
+                overlay.cell(w, h=line_height, txt=part)
+                line_len += w
+            overlay.ln()
+            max_line_len = max(max_line_len, line_len)
+            
+        return max_line_len
 
     def _draw_qr(self, overlay, url):
         qr = qrcode.QRCode(box_size=4)
