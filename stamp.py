818fad8986ad2d0c8da3a4592dd4bef8365f5864
Matthias Valvekens <matthias@mvalvekens.be>
2020-08-09 16:03:30 +0200

    Factor text handling code out of QRStamp

--- stamp.py
+++ stamp.py
@@ -54,40 +54,40 @@ class PdfStreamImage(BaseImage):
 
 
 @dataclass(frozen=True)
-class StampStyle:
+class TextStampStyle:
+    stamp_text: str
     font: Optional[Union[str, TTFont]] = None
     font_size: int = 10
-    innsep: int = 3
+    leading: int = None
     textsep: int = 10
     max_text_width: int = 300
     avg_char_width: int = None
-
-    stamp_qrsize: int = 100
-
     timestamp_format: str = '%Y-%m-%d %H:%M:%S %Z'
 
+
+@dataclass(frozen=True)
+class QRStampStyle(TextStampStyle):
+    innsep: int = 3
     stamp_text: str = (
         "Digital version available at\n"
         "this url: %(url)s\n"
         "Timestamp: %(ts)s"
     )
+    stamp_qrsize: int = 100
 
 
-class QRStamp(generic.StreamObject):
-
-    def __init__(self, writer: IncrementalPdfFileWriter, url: str, 
-                 style: StampStyle, text_params=None):
+class TextStamp(generic.StreamObject):
+    def __init__(self, writer: IncrementalPdfFileWriter, style,
+                 text_params=None):
         super().__init__()
         self.writer = writer
-        self.url = url
         self.style = style
-        self.text_params = None
+        self.text_params = text_params
         self.update({
             pdf_name('/Type'): pdf_name('/XObject'),
             pdf_name('/Subtype'): pdf_name('/Form')
         })
         self._resources_ready = False
-        self.text_params = text_params
         self.font = font = self.style.font or 'Courier'
 
         if isinstance(font, TTFont):
@@ -108,6 +108,12 @@ class QRStamp(generic.StreamObject):
             char_width = self.style.avg_char_width or 0.6 * self.style.font_size
             return '(%s)' % txt, len(txt) * char_width
 
+    def add_resources(self, resources):
+        pass
+
+    def extra_commands(self):
+        return []
+
     def _format_resources(self):
         if self._resources_ready:
             return
@@ -124,16 +130,155 @@ class QRStamp(generic.StreamObject):
         else:
             font_ref = self.glyph_accumulator.embed_subset(self.writer)
 
-        resources = generic.DictionaryObject({
+        raw_resource_dict = {
             pdf_name('/Font'): generic.DictionaryObject({
                 pdf_name('/F1'): font_ref
             }),
-            pdf_name('/XObject'): generic.DictionaryObject({
-                pdf_name('/QR'): self._qr_xobject()
-            })
-        })
+        }
+        self.add_resources(raw_resource_dict)
+        resources = generic.DictionaryObject(raw_resource_dict)
         self[pdf_name('/Resources')] = self.writer.add_object(resources)
         self._resouces_ready = True
+
+    def get_text_sep(self):
+        return self.style.textsep
+
+    def get_leading(self):
+        style = self.style
+        return style.font_size if style.leading is None else style.leading
+
+    def get_text_height(self):
+        return len(self.style.stamp_text.split('\n')) * self.get_leading()
+
+    def get_stamp_height(self):
+        return self.get_text_height() + 2 * self.get_text_sep()
+
+    def get_text_xstart(self):
+        return self.get_text_sep()
+
+    def get_text_ystart(self):
+        return (self.get_text_height() + self.get_stamp_height()) // 2
+
+    def get_default_text_params(self):
+        ts = datetime.now(tz=tzlocal.get_localzone())
+        return {
+            'ts': ts.strftime(self.style.timestamp_format),
+        }
+
+    def _render_stream(self):
+        style = self.style
+
+        command_stream = ['q']
+
+        # The width depends on the width of the inner text box
+        #  => compute later
+        stamp_height = self.get_stamp_height()
+        # text rendering
+        max_line_len, text_commands = self._text_stream()
+        command_stream.append(text_commands)
+
+        # append additional drawing commands
+        command_stream.extend(self.extra_commands())
+
+        stamp_width = (
+            self.get_text_xstart() + min(style.max_text_width, max_line_len)
+            + self.get_text_sep()
+        )
+
+        # draw border around stamp and set bounding box
+        self[pdf_name('/BBox')] = generic.ArrayObject(list(
+            map(generic.FloatObject, [0, 0, stamp_width, stamp_height])
+        ))
+        command_stream.append(
+            '3 w 0 0 %g %g re S' % (stamp_width, stamp_height)
+        )
+        command_stream.append('Q')
+        self._data = ' '.join(command_stream).encode('latin-1')
+        return stamp_width, stamp_height
+
+    def _text_stream(self):
+        style = self.style
+        leading = style.font_size if style.leading is None else style.leading
+        xstart = self.get_text_xstart()
+        ystart = self.get_text_ystart()
+
+        # render text
+        max_line_len = 0
+        _text_params = self.get_default_text_params()
+        if self.text_params is not None:
+            _text_params.update(self.text_params)
+        text = style.stamp_text % _text_params
+
+        # TODO Auto word-wrap is probably too much trouble, but
+        #  perhaps it's worth experimenting a little
+        self.glyph_accumulator.feed_string(text)
+        command_stream = [
+            'BT', '/F1 %d Tf' % self.style.font_size,
+                  '%d TL' % leading, '%d %d Td' % (xstart, ystart)
+        ]
+        for line in text.split('\n'):
+            wrapped_line, line_len = self.wrap_string(line)
+            max_line_len = max(max_line_len, line_len)
+            command_stream.append("%s '" % wrapped_line)
+        command_stream.append('ET')
+        return max_line_len, ' '.join(command_stream)
+
+    def render_all(self):
+        w, h = self._render_stream()
+        self._format_resources()
+        return w, h
+
+    def writeToStream(self, stream, key):
+        if self._data is None:
+            raise ValueError(
+                'Stamp stream needs to be rendered before calling .write()'
+            )
+        return super().writeToStream(stream, key)
+
+    def apply(self, dest_page, x, y):
+        stamp_ref = self.writer.add_object(self)
+        # randomise resource name to avoid conflicts
+        resource_name = '/Stamp' + os.urandom(16).hex()
+        stamp_paint = 'q 1 0 0 1 %g %g cm %s Do Q' % (
+            rd(x), rd(y), resource_name
+        )
+        stamp_wrapper_stream = generic.StreamObject.initializeFromDictionary({
+            '__streamdata__': stamp_paint.encode('ascii'),
+            pdf_name('/Length'): len(stamp_paint)
+        })
+        resources = generic.DictionaryObject({
+            pdf_name('/XObject'): generic.DictionaryObject({
+                pdf_name(resource_name): stamp_ref
+            })
+        })
+        wr = self.writer
+        page_ref = wr.add_stream_to_page(
+            dest_page, wr.add_object(stamp_wrapper_stream), resources
+        )
+        return stamp_ref, page_ref, self.render_all()
+
+
+class QRStamp(TextStamp):
+
+    def __init__(self, writer: IncrementalPdfFileWriter, url: str,
+                 style: QRStampStyle, text_params=None):
+        super().__init__(writer, style, text_params=text_params)
+        self.url = url
+
+    def add_resources(self, raw_resource_dict):
+        raw_resource_dict[pdf_name('/XObject')] = generic.DictionaryObject({
+            pdf_name('/QR'): self._qr_xobject()
+        })
+
+    def extra_commands(self):
+        height = self.get_stamp_height()
+        qr_y_sep = (height - self.style.stamp_qrsize) // 2
+        # paint the QR code, translated and with y axis inverted
+        draw_qr_command = 'q 1 0 0 -1 %g %g cm /QR Do Q' % (
+            rd(self.style.innsep),
+            rd(height - max(self.style.innsep, qr_y_sep)),
+        )
+        return [draw_qr_command]
 
     def _qr_xobject(self):
         qr = qrcode.QRCode(box_size=4)
@@ -153,127 +298,23 @@ class QRStamp(generic.StreamObject):
         )
         return self.writer.add_object(qr_xobj)
 
-    def _render_stream(self):
-        style = self.style
+    def get_text_xstart(self):
+        return 2 * self.style.innsep + self.style.stamp_qrsize
 
-        # inner text area geometry
-        total_text_sep = style.innsep + style.textsep
-        text_min_x = 2 * style.innsep + style.stamp_qrsize
-        # roughly estimate text height
-        text_height = len(style.stamp_text.split('\n')) * style.font_size
-
-        command_stream = ['q']
-
+    def get_stamp_height(self):
         # height is determined by the height of the text,
         # or the QR code, whichever is greater
-        # The width depends on the width of the inner text box
-        #  => compute later
+        sh = super().get_stamp_height()
+        return max(sh, self.style.stamp_qrsize + 2 * self.style.innsep)
 
-        stamp_height = max(
-            text_height + 2 * total_text_sep,
-            style.stamp_qrsize + 2 * style.innsep
-        )
-        self._height = stamp_height
-        text_y_sep = (stamp_height - text_height) // 2
+    def get_default_text_params(self):
+        tp = super().get_default_text_params()
+        tp['url'] = self.url
+        return tp
 
-        # text rendering
-        max_line_len, text_commands = self._text_stream(
-            text_min_x, stamp_height - text_y_sep
-        )
-
-        stamp_width = (
-            text_min_x + min(style.max_text_width, max_line_len)
-            + total_text_sep
-        )
-        self._width = stamp_width
-
-        qr_y_sep = (stamp_height - style.stamp_qrsize) // 2
-
-        command_stream.append(text_commands)
-        # paint the QR code, translated and with y axis inverted
-        command_stream.append(
-            'q 1 0 0 -1 %g %g cm /QR Do Q' % (
-                rd(style.innsep),
-                rd(stamp_height - max(style.innsep, qr_y_sep)),
-            )
-        )
-
-        # draw border around stamp and set bounding box
-        self[pdf_name('/BBox')] = generic.ArrayObject(list(
-            map(generic.FloatObject, [0, 0, stamp_width, stamp_height])
-        ))
-        command_stream.append(
-            '3 w 0 0 %g %g re S' % (stamp_width, stamp_height)
-        )
-        command_stream.append('Q')
-        self._data = ' '.join(command_stream).encode('latin-1')
-
-    def _text_stream(self, xstart, ystart):
-        style = self.style
-        line_height = style.font_size
-
-        # render text
-        max_line_len = 0
-        ts = datetime.now(tz=tzlocal.get_localzone())
-        _text_params = {
-            'ts': ts.strftime(style.timestamp_format),
-            'url': self.url
-        }
-        if self.text_params is not None:
-            _text_params.update(self.text_params)
-        text = style.stamp_text % _text_params
-
-        # TODO Auto word-wrap is probably too much trouble, but
-        #  perhaps it's worth experimenting a little
-        self.glyph_accumulator.feed_string(text)
-        command_stream = [
-            'BT', '/F1 %d Tf' % self.style.font_size,
-            '%d TL' % line_height, '%d %d Td' % (xstart, ystart)
-        ]
-        for line in text.split('\n'):
-            wrapped_line, line_len = self.wrap_string(line)
-            max_line_len = max(max_line_len, line_len)
-            command_stream.append('%s Tj T*' % wrapped_line)
-        command_stream.append('ET')
-        return max_line_len, ' '.join(command_stream)
-
-    def render_all(self):
-        self._render_stream()
-        self._format_resources()
-
-    def writeToStream(self, stream, key):
-        if self._data is None:
-            raise ValueError(
-                'Stamp stream needs to be rendered before calling .write()'
-            )
-        return super().writeToStream(stream, key)
-
-    @classmethod
-    def apply(cls, writer: IncrementalPdfFileWriter, style, dest_page,
-              x, y, url, text_params=None):
-
-        stamp = cls(writer, url, style, text_params)
-        stamp_ref = writer.add_object(stamp)
-        # randomise resource name to avoid conflicts
-        resource_name = '/QRStamp' + os.urandom(16).hex()
-        stamp_paint = 'q 1 0 0 1 %g %g cm %s Do Q' % (
-            rd(x), rd(y), resource_name
-        )
-        stamp_wrapper_stream = generic.StreamObject.initializeFromDictionary({
-            '__streamdata__': stamp_paint.encode('ascii'),
-            pdf_name('/Length'): len(stamp_paint)
-        })
-        resources = generic.DictionaryObject({
-            pdf_name('/XObject'): generic.DictionaryObject({
-                pdf_name(resource_name): stamp_ref
-            })
-        })
-        page_ref = writer.add_stream_to_page(
-            dest_page, writer.add_object(stamp_wrapper_stream), resources
-        )
-        stamp.render_all()
-
-        link_rect = (x, y, x + stamp._width, y + stamp._height)
+    def apply(self, dest_page, x, y):
+        stamp_ref, page_ref, (w, h) = super().apply(dest_page, x, y)
+        link_rect = (x, y, x + w, y + h)
         link_annot = generic.DictionaryObject({
             pdf_name('/Type'): pdf_name('/Annot'),
             pdf_name('/Subtype'): pdf_name('/Link'),
@@ -282,11 +323,12 @@ class QRStamp(generic.StreamObject):
             )),
             pdf_name('/A'): generic.DictionaryObject({
                 pdf_name('/S'): pdf_name('/URI'),
-                pdf_name('/URI'): pdf_string(url)
+                pdf_name('/URI'): pdf_string(self.url)
             })
         })
-        writer.register_annotation(page_ref, writer.add_object(link_annot))
-        return stamp_ref
+        wr = self.writer
+        wr.register_annotation(page_ref, wr.add_object(link_annot))
+        return stamp_ref, page_ref, (w, h)
 
 
 def stamp_file(input_name, output_name, style, dest_page,
@@ -294,7 +336,8 @@ def stamp_file(input_name, output_name, style, dest_page,
 
     with open(input_name, 'rb') as fin:
         pdf_out = IncrementalPdfFileWriter(fin)
-        QRStamp.apply(pdf_out, style, dest_page, x, y, url, text_params)
+        stamp = QRStamp(pdf_out, url, style, text_params=text_params)
+        stamp.apply(dest_page, x, y)
 
         with open(output_name, 'wb') as out:
             pdf_out.write(out)
