fbb6c2fadce740d7146dda81e21f6d839f4376cd
Matthias Valvekens <matthias@mvalvekens.be>
2020-08-24 09:47:53 +0200

    Basic font embedding test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,11 @@
 import pytest
 from io import BytesIO
 
+from pdf_utils.incremental_writer import IncrementalPdfFileWriter
 from pdf_utils.reader import PdfFileReader
 from pdf_utils import writer
+from fontTools import ttLib
+from pdf_utils.font import GlyphAccumulator
 
 from .samples import *
 
@@ -26,3 +29,16 @@ def test_create_fresh(zip1, zip2):
     kids = pages['/Kids']
     assert b'world' in kids[0].get_object()['/Contents'].data
     assert b'Page 2' in kids[1].get_object()['/Contents'].data
+
+
+def test_embed_subset():
+    ffile = ttLib.TTFont('tests/data/fonts/NotoSerifJP-Regular.otf')
+    ga = GlyphAccumulator(ffile)
+    cid_hx, _ = ga.feed_string('版')
+    assert cid_hx == '66eb'
+    cid_hx, _ = ga.feed_string('テスト版')
+    assert cid_hx == '0637062a063966eb'
+    w = IncrementalPdfFileWriter(BytesIO(MINIMAL))
+    font_ref = ga.embed_subset(w)
+    cid_font = font_ref.get_object()['/DescendantFonts'][0].get_object()
+    assert '/FontFile3' in cid_font['/FontDescriptor']
