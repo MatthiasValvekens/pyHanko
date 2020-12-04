ec95af9022a8fed2028857bc493f5e82f35f3775
Matthias Valvekens <matthias@mvalvekens.be>
2020-12-04 17:30:47 +0100

    Fix CRL start time using libfreezetime

--- pyhanko_tests/data/crypto/testing-ca-setup/ca-setup.sh
+++ pyhanko_tests/data/crypto/testing-ca-setup/ca-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Set up a CA for testing purposes using openssl
+# Set up a CA for testing purposes using "$OPENSSL" and faketime
+
+# NOTE: on macos, you need to set the "$OPENSSL" variable to a 
+# non-SIP-protected binary for the time manipulation tricks to work
 
 set -e
 set -o pipefail
@@ -11,7 +14,7 @@ subj_prefix="/C=$COUNTRY/O=$ORG/OU=$ORG_UNIT"
 ensure_key () {
     if [[ "$FORCE_NEW_KEYS" = yes || ! -f "$1" ]] ; then
         echo -n "Generating RSA key for $1... "
-        openssl genrsa -aes256 -out "$1" -passout "pass:$DUMMY_PASSWORD" \
+        "$OPENSSL" genrsa -aes256 -out "$1" -passout "pass:$DUMMY_PASSWORD" \
             2>> "$LOGFILE" >/dev/null
         echo "OK"
     fi
@@ -56,13 +59,13 @@ echo "Setting up root certificate authority..."
 setup_ca_dir root
 
 if [[ "$FORCE_NEW_CERTS" = yes || ! -f "root/certs/ca.cert.pem" ]] ; then
-    openssl req -config openssl.cnf -key root/ca.key.pem \
+    "$OPENSSL" req -config openssl.cnf -key root/ca.key.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -subj "$subj_prefix/CN=Root CA" \
         -out "root/csr/root.csr.pem" -new -sha256 \
         2>> "$LOGFILE" >/dev/null
 
-    openssl ca -batch -config openssl.cnf -name CA_root \
+    "$OPENSSL" ca -batch -config openssl.cnf -name CA_root \
         -passin "pass:$DUMMY_PASSWORD" -selfsign \
         -in root/csr/root.csr.pem \
         -out root/certs/ca.cert.pem -md sha256 -notext \
@@ -76,13 +79,13 @@ setup_ca_dir intermediate
 
 if [[ "$FORCE_NEW_CERTS" = yes || ! -f "intermediate/certs/ca.cert.pem" ]]
 then
-    openssl req -config openssl.cnf -key intermediate/ca.key.pem \
+    "$OPENSSL" req -config openssl.cnf -key intermediate/ca.key.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -subj "$subj_prefix/CN=Intermediate CA" \
         -out "root/csr/intermediate.csr.pem" -new -sha256 \
         2>> "$LOGFILE" >/dev/null
 
-    openssl ca -batch -config openssl.cnf -name CA_root \
+    "$OPENSSL" ca -batch -config openssl.cnf -name CA_root \
         -extensions v3_intermediate_ca -md sha256 -notext \
         -startdate $INTERM_START -enddate $INTERM_END \
         -passin "pass:$DUMMY_PASSWORD" \
@@ -100,13 +103,13 @@ if [[ "$FORCE_NEW_CERTS" = yes || ! -f "$LEAF_CERTS/ocsp.cert.pem" ]]
 then
     echo "Signing OCSP responder certificate for intermediate CA..."
     ensure_key keys/ocsp.key.pem 
-    openssl req -config openssl.cnf -key keys/ocsp.key.pem \
+    "$OPENSSL" req -config openssl.cnf -key keys/ocsp.key.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -subj "$subj_prefix/CN=OCSP Responder" \
         -out intermediate/csr/ocsp.csr.pem -new -sha256 \
         2>> "$LOGFILE" >/dev/null
 
-    openssl ca -batch -config openssl.cnf -name CA_intermediate \
+    "$OPENSSL" ca -batch -config openssl.cnf -name CA_intermediate \
         -extensions ocsp -md sha256 -notext \
         -startdate $OCSP_START -enddate $OCSP_END \
         -passin "pass:$DUMMY_PASSWORD" \
@@ -121,13 +124,13 @@ if [[ "$FORCE_NEW_CERTS" = yes || ! -f "$LEAF_CERTS/tsa.cert.pem" ]]
 then
     echo "Signing TSA certificate..."
     ensure_key keys/tsa.key.pem 
-    openssl req -config openssl.cnf -key keys/tsa.key.pem \
+    "$OPENSSL" req -config openssl.cnf -key keys/tsa.key.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -subj "$subj_prefix/CN=Time Stamping Authority" \
         -out root/csr/tsa.csr.pem -new -sha256 \
         2>> "$LOGFILE" >/dev/null
 
-    openssl ca -batch -config openssl.cnf -name CA_root \
+    "$OPENSSL" ca -batch -config openssl.cnf -name CA_root \
         -extensions tsa_cert -md sha256 -notext \
         -startdate $TSA_START -enddate $TSA_END \
         -passin "pass:$DUMMY_PASSWORD" \
@@ -141,13 +144,13 @@ if [[ "$FORCE_NEW_CERTS" = yes || ! -f "$LEAF_CERTS/$SIGNER_IDENT.cert.pem" ]]
 then
     echo "Signing end-user certificate for $SIGNER_NAME..."
     ensure_key keys/$SIGNER_IDENT.key.pem
-    openssl req -config openssl.cnf -key keys/$SIGNER_IDENT.key.pem \
+    "$OPENSSL" req -config openssl.cnf -key keys/$SIGNER_IDENT.key.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -subj "$subj_prefix/CN=$SIGNER_NAME/emailAddress=$SIGNER_EMAIL" \
         -out intermediate/csr/$SIGNER_IDENT.csr.pem -new -sha256 \
         2>> "$LOGFILE" >/dev/null
 
-    openssl ca -batch -config openssl.cnf -name CA_intermediate \
+    "$OPENSSL" ca -batch -config openssl.cnf -name CA_intermediate \
         -extensions usr_cert -md sha256 -notext \
         -startdate $SIGNER_START -enddate $SIGNER_END \
         -passin "pass:$DUMMY_PASSWORD" \
@@ -155,7 +158,7 @@ then
         -out $LEAF_CERTS/$SIGNER_IDENT.cert.pem \
         2>> "$LOGFILE" >/dev/null
 
-    openssl pkcs12 -export -out $LEAF_CERTS/$SIGNER_IDENT.pfx \
+    "$OPENSSL" pkcs12 -export -out $LEAF_CERTS/$SIGNER_IDENT.pfx \
         -inkey keys/$SIGNER_IDENT.key.pem \
         -in $LEAF_CERTS/$SIGNER_IDENT.cert.pem \
         -certfile intermediate/certs/ca-chain.cert.pem \
@@ -168,13 +171,13 @@ if [[ "$FORCE_NEW_CERTS" = yes || ! -f "$LEAF_CERTS/$SIGNER_IDENT2.cert.pem" ]]
 then
     echo "Signing end-user certificate for $SIGNER2_NAME..."
     ensure_key keys/$SIGNER2_IDENT.key.pem
-    openssl req -config openssl.cnf -key keys/$SIGNER2_IDENT.key.pem \
+    "$OPENSSL" req -config openssl.cnf -key keys/$SIGNER2_IDENT.key.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -subj "$subj_prefix/CN=$SIGNER2_NAME/emailAddress=$SIGNER2_EMAIL" \
         -out intermediate/csr/$SIGNER2_IDENT.csr.pem -new -sha256 \
         2>> "$LOGFILE" >/dev/null
 
-    openssl ca -batch -config openssl.cnf -name CA_intermediate \
+    "$OPENSSL" ca -batch -config openssl.cnf -name CA_intermediate \
         -extensions usr_cert -md sha256 -notext \
         -startdate $SIGNER2_START -enddate $SIGNER2_END \
         -passin "pass:$DUMMY_PASSWORD" \
@@ -182,7 +185,7 @@ then
         -out $LEAF_CERTS/$SIGNER2_IDENT.cert.pem \
         2>> "$LOGFILE" >/dev/null
 
-    openssl pkcs12 -export -out $LEAF_CERTS/$SIGNER2_IDENT.pfx \
+    "$OPENSSL" pkcs12 -export -out $LEAF_CERTS/$SIGNER2_IDENT.pfx \
         -inkey keys/$SIGNER2_IDENT.key.pem \
         -in $LEAF_CERTS/$SIGNER2_IDENT.cert.pem \
         -certfile intermediate/certs/ca-chain.cert.pem \
@@ -207,7 +210,8 @@ fi
 if [[ "$FORCE_NEW_CERTS" = yes || ! -f "intermediate/crl/ca.crl.pem" ]]
 then
     ensure_dir intermediate/crl
-    openssl ca -name CA_intermediate -config openssl.cnf -gencrl \
+    faketime "$CRL_DATE" "$OPENSSL" ca -name CA_intermediate \
+        -config openssl.cnf -gencrl \
         -out intermediate/crl/ca.crl.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -crldays $CRLDAYS \
@@ -217,7 +221,8 @@ fi
 if [[ "$FORCE_NEW_CERTS" = yes || ! -f "root/crl/ca.crl.pem" ]]
 then
     ensure_dir root/crl
-    openssl ca -name CA_root -config openssl.cnf -gencrl \
+    faketime "$CRL_DATE" "$OPENSSL" ca -name CA_root \
+        -config openssl.cnf -gencrl \
         -out root/crl/ca.crl.pem \
         -passin "pass:$DUMMY_PASSWORD" \
         -crldays $CRLDAYS \
