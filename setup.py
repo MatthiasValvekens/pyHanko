b090af98d454a2d4e9e33859efbdcd8105096231
Matthias Valvekens <matthias@mvalvekens.be>
2021-10-11 13:43:17 +0200

    Bump pyhanko-certvalidator version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'pytz>=2020.1',
         'qrcode>=6.1',
         'tzlocal>=2.1',
-        'pyhanko-certvalidator~=0.16.0',
+        'pyhanko-certvalidator~=0.17.1',
         'click>=7.1.2',
         'requests>=2.24.0',
         'pyyaml>=5.3.1',
