1d85f2d429d887079d7092950ab6e719a64d72a5
Matthias Valvekens <matthias@mvalvekens.be>
2021-11-12 15:54:01 +0100

    Add python-pae to test requirements

--- setup.py
+++ setup.py
@@ -82,7 +82,8 @@ setup(
     tests_require=[
         'pytest>=6.1.1', 'requests-mock>=1.8.0',
         'freezegun>=1.1.0', 'certomancer~=0.6.0',
-        'aiohttp>=3.7.4', 'pytest-aiohttp~=0.3.0'
+        'aiohttp~=3.8.0', 'pytest-aiohttp~=0.3.0',
+        'python-pae==0.1.0'
     ],
     keywords="signature pdf pades digital-signature pkcs11"
 )
