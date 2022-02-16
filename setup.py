9b6e960c27e9299258ca6867854b8b9e1606da0b
Matthias Valvekens <matthias@mvalvekens.be>
2022-02-16 23:30:22 +0100

    Bump Certomancer version to 0.8.1

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     },
     tests_require=[
         'pytest>=6.1.1', 'requests-mock>=1.8.0',
-        'freezegun>=1.1.0', 'certomancer~=0.8.0',
+        'freezegun>=1.1.0', 'certomancer~=0.8.1',
         'aiohttp~=3.8.0', 'pytest-aiohttp~=0.3.0',
         'certomancer-csc-dummy==0.1.0'
     ],
