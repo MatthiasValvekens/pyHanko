5897ad3c6546703b27a33844d8207a83e71cfe1d
Matthias Valvekens <matthias@mvalvekens.be>
2022-10-02 09:31:38 +0200

    Make it easier to extract keys from bytes
    Fixes #160.

--- pyhanko/sign/general.py
+++ pyhanko/sign/general.py
@@ -399,25 +399,38 @@ def load_certs_from_pemder(cert_files):
     :return:
         A generator producing :class:`.asn1crypto.x509.Certificate` objects.
     """
-    for ca_chain_file in cert_files:
-        with open(ca_chain_file, 'rb') as f:
-            ca_chain_bytes = f.read()
-        # use the pattern from the asn1crypto docs
-        # to distinguish PEM/DER and read multiple certs
-        # from one PEM file (if necessary)
-        if pem.detect(ca_chain_bytes):
-            pems = pem.unarmor(ca_chain_bytes, multiple=True)
-            for type_name, _, der in pems:
-                if type_name is None or type_name.lower() == 'certificate':
-                    yield x509.Certificate.load(der)
-                else:  # pragma: nocover
-                    logger.debug(
-                        f'Skipping PEM block of type {type_name} in '
-                        f'CA chain file.'
-                    )
-        else:
-            # no need to unarmor, just try to load it immediately
-            yield x509.Certificate.load(ca_chain_bytes)
+    for cert_file in cert_files:
+        with open(cert_file, 'rb') as f:
+            cert_data_bytes = f.read()
+        yield from load_certs_from_pemder_data(cert_data_bytes)
+
+
+def load_certs_from_pemder_data(cert_data_bytes: bytes):
+    """
+    A convenience function to load PEM/DER-encoded certificates from
+    binary data.
+
+    :param cert_data_bytes:
+        ``bytes`` object from which to extract certificates.
+    :return:
+        A generator producing :class:`.asn1crypto.x509.Certificate` objects.
+    """
+    # use the pattern from the asn1crypto docs
+    # to distinguish PEM/DER and read multiple certs
+    # from one PEM file (if necessary)
+    if pem.detect(cert_data_bytes):
+        pems = pem.unarmor(cert_data_bytes, multiple=True)
+        for type_name, _, der in pems:
+            if type_name is None or type_name.lower() == 'certificate':
+                yield x509.Certificate.load(der)
+            else:  # pragma: nocover
+                logger.debug(
+                    f'Skipping PEM block of type {type_name} in '
+                    f'CA chain file.'
+                )
+    else:
+        # no need to unarmor, just try to load it immediately
+        yield x509.Certificate.load(cert_data_bytes)
 
 
 def load_cert_from_pemder(cert_file):
@@ -452,6 +465,22 @@ def load_private_key_from_pemder(key_file, passphrase: Optional[bytes]) \
     """
     with open(key_file, 'rb') as f:
         key_bytes = f.read()
+    return load_private_key_from_pemder_data(key_bytes, passphrase=passphrase)
+
+
+def load_private_key_from_pemder_data(
+        key_bytes: bytes,
+        passphrase: Optional[bytes]) -> keys.PrivateKeyInfo:
+    """
+    A convenience function to load PEM/DER-encoded keys from binary data.
+
+    :param key_bytes:
+        ``bytes`` object to read the key from.
+    :param passphrase:
+        Key passphrase.
+    :return:
+        A private key encoded as an unencrypted PKCS#8 PrivateKeyInfo object.
+    """
     load_fun = (
         serialization.load_pem_private_key if pem.detect(key_bytes)
         else serialization.load_der_private_key
