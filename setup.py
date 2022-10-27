f73cb0e199684807e4e44d6c9f86d795c1179566
Matthias Valvekens <matthias@mvalvekens.be>
2022-10-27 22:30:02 +0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -115,7 +115,9 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
 
         'Topic :: Security :: Cryptography',
     ],
