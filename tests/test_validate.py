9ea5612c89cbb6f1461929f99919a889ffaaa0e1
Matthias Valvekens <matthias@mvalvekens.be>
2023-10-05 23:26:52 +0200

    Fix non-discoverable OCSP tests

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -422,7 +422,7 @@ def read_openssl_ocsp_test_params():
 @pytest.mark.parametrize(
     "test_case", read_openssl_ocsp_test_params(), ids=lambda case: case.name
 )
-def openssl_ocsp(test_case: OCSPTestCase):
+def test_openssl_ocsp(test_case: OCSPTestCase):
     context = ValidationContext(
         trust_roots=test_case.roots,
         other_certs=test_case.other_certs,
