bde915f8750853cc51eee406c3b940293d2cdc70
Matthias Valvekens <matthias@mvalvekens.be>
2023-12-10 23:32:46 +0100

    Qualification algorithm: beginnings

--- pyhanko/sign/validation/eutl.py
+++ pyhanko/sign/validation/eutl.py
@@ -1,7 +1,8 @@
 import enum
 import logging
+import zoneinfo
 from collections import defaultdict
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from datetime import datetime
 from typing import (
     Dict,
@@ -19,6 +20,7 @@ from typing import (
 from asn1crypto import x509
 from pyhanko_certvalidator.authority import Authority, AuthorityWithCert
 from pyhanko_certvalidator.errors import InvalidCertificateError
+from pyhanko_certvalidator.path import ValidationPath
 from xsdata.formats.dataclass.parsers import XmlParser
 from xsdata.formats.dataclass.parsers.config import ParserConfig
 
@@ -28,13 +30,39 @@ from pyhanko.generated.etsi import (
     ts_119612_sie,
     xades,
 )
+from pyhanko.sign.ades import qualified_asn1
 from pyhanko.sign.validation.settings import KeyUsageConstraints
+
+__all__ = [
+    'TSPRegistry',
+    'CAServiceInformation',
+    'QualificationAssessor',
+    'QualifiedStatus',
+    'read_qualified_certificate_authorities',
+    'AdditionalServiceInformation',
+    'QcCertType',
+    'BaseServiceInformation',
+    'Criterion',
+    'KeyUsageCriterion',
+    'PolicySetCriterion',
+    'CertSubjectDNCriterion',
+    'CriteriaCombination',
+    'CriteriaList',
+    'Qualifier',
+    'Qualification',
+    'QcPrivateKeyManagementType',
+    'TSPServiceParsingError',
+]
+
 
 logger = logging.getLogger(__name__)
 
-CA_QC_URI = 'http://uri.etsi.org/TrstSvc/Svctype/CA/QC'
-QTST_URI = 'http://uri.etsi.org/TrstSvc/Svctype/TSA/QTST'
-STATUS_GRANTED = 'http://uri.etsi.org/TrstSvc/TrustedList/Svcstatus/granted'
+# noinspection HttpUrlsUsage
+_TRSTSVC_URI_BASE = 'http://uri.etsi.org/TrstSvc'
+_TRUSTEDLIST_URI_BASE = f'{_TRSTSVC_URI_BASE}/TrustedList'
+CA_QC_URI = f'{_TRSTSVC_URI_BASE}/Svctype/CA/QC'
+QTST_URI = f'{_TRSTSVC_URI_BASE}/Svctype/TSA/QTST'
+STATUS_GRANTED = f'{_TRUSTEDLIST_URI_BASE}/Svcstatus/granted'
 
 
 class TSPServiceParsingError(ValueError):
@@ -48,12 +76,28 @@ class AdditionalServiceInformation:
     textual_info: Optional[str]
 
 
+class QcCertType(enum.Enum):
+    QC_ESIGN = 'qct_esign'
+    QC_ESEAL = 'qct_eseal'
+    QC_WEB = 'qct_web'
+
+
+_SVCINFOEXT_URI_BASE = f'{_TRUSTEDLIST_URI_BASE}/SvcInfoExt'
+
+_CERTIFICATE_TYPE_BY_URI = {
+    f'{_SVCINFOEXT_URI_BASE}/ForeSignatures': QcCertType.QC_ESIGN,
+    f'{_SVCINFOEXT_URI_BASE}/ForeSeals': QcCertType.QC_ESEAL,
+    f'{_SVCINFOEXT_URI_BASE}/ForWebSiteAuthentication': QcCertType.QC_WEB,
+}
+
+
 @dataclass(frozen=True)
 class BaseServiceInformation:
     service_type: str
     service_name: str
     provider_certs: Tuple[x509.Certificate, ...]
-    additional_info: FrozenSet[AdditionalServiceInformation]
+    additional_info_certificate_type: FrozenSet[QcCertType]
+    other_additional_info: FrozenSet[AdditionalServiceInformation]
 
 
 class Qualifier(enum.Enum):
@@ -63,7 +107,7 @@ class Qualifier(enum.Enum):
     WITH_QSCD = 'QCWithQSCD'
     NO_QSCD = 'QCNoQSCD'
     QSCD_AS_IN_CERT = 'QCQSCDStatusAsInCert'
-    QSCD_MANAGED = 'QCQSCDManagedOnBehalf'
+    QSCD_MANAGED_ON_BEHALF = 'QCQSCDManagedOnBehalf'
     LEGAL_PERSON = 'QCForLegalPerson'
     FOR_ESIG = 'QCForEsig'
     FOR_ESEAL = 'QCForEseal'
@@ -73,12 +117,10 @@ class Qualifier(enum.Enum):
 
     @property
     def uri(self):
-        return (
-            f"http://uri.etsi.org/TrstSvc/TrustedList/SvcInfoExt/{self.value}"
-        )
+        return f"{_SVCINFOEXT_URI_BASE}/{self.value}"
 
 
-_BY_URI = {q.uri: q for q in Qualifier}
+_QUALIFIER_BY_URI = {q.uri: q for q in Qualifier}
 
 
 class Criterion:
@@ -272,7 +314,7 @@ def _process_criteria_list_entries(
 def _process_qualifiers(qualifiers: Iterable[ts_119612_sie.QualifierType]):
     for qual in qualifiers:
         try:
-            yield _BY_URI[qual.uri]
+            yield _QUALIFIER_BY_URI[qual.uri]
         except KeyError:
             logger.info(f"Qualifier {qual.uri} in SDI ignored...")
 
@@ -327,6 +369,7 @@ def _interpret_service_info_for_ca(
         if service_info.service_information_extensions
         else ()
     )
+    asi_qc_type: Set[QcCertType] = set()
     for ext in extensions_xml:
         for ext_content in ext.content:
             if isinstance(ext_content, ts_119612_sie.Qualifications):
@@ -339,9 +382,15 @@ def _interpret_service_info_for_ca(
             elif isinstance(
                 ext_content, ts_119612.AdditionalServiceInformation
             ):
-                additional_info.append(
-                    _process_additional_info(ext_content, ext.critical or False)
+                additional_info_entry = _process_additional_info(
+                    ext_content, ext.critical or False
                 )
+                try:
+                    asi_qc_type.add(
+                        _CERTIFICATE_TYPE_BY_URI[additional_info_entry.uri]
+                    )
+                except KeyError:
+                    additional_info.append(additional_info_entry)
             elif ext.critical:
                 # TODO more informative exception / only ditch the current SDI
                 raise TSPServiceParsingError(
@@ -355,7 +404,8 @@ def _interpret_service_info_for_ca(
         ),
         service_name=service_name or "unknown",
         provider_certs=tuple(certs),
-        additional_info=frozenset(additional_info),
+        additional_info_certificate_type=frozenset(asi_qc_type),
+        other_additional_info=frozenset(additional_info),
     )
 
     return CAServiceInformation(
@@ -382,6 +432,7 @@ def _interpret_service_info_for_cas(
         #  work, store that info on the object
         if service_info.service_status != STATUS_GRANTED:
             continue
+        # TODO process errors in individual services
         yield _interpret_service_info_for_ca(service)
 
 
@@ -425,3 +476,255 @@ class TSPRegistry:
         self, ca: Authority
     ) -> Iterable[CAServiceInformation]:
         return tuple(self._cert_to_si[ca])
+
+    # TODO take date into account (and properly track it
+    #  for service definitions)
+    def applicable_tsps_on_path(
+        self,
+        path: ValidationPath,
+    ) -> Generator[CAServiceInformation, None, None]:
+        for ca in path.iter_authorities():
+            yield from self.applicable_service_definitions(ca)
+
+
+class QcPrivateKeyManagementType(enum.Enum):
+    UNKNOWN = 0
+    QCSD = 1
+    QCSD_DELEGATED = 2
+    QCSD_BY_POLICY = 3
+
+    @property
+    def is_qcsd(self) -> bool:
+        return self != QcPrivateKeyManagementType.UNKNOWN
+
+
+EIDAS_START_DATE = datetime(
+    2016, 7, 1, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo('CET')
+)
+
+PRE_EIDAS_QCP_POLICY = '0.4.0.1456.1.1'
+PRE_EIDAS_QCP_PLUS_POLICY = '0.4.0.1456.1.2'
+
+
+@dataclass(frozen=True)
+class QualifiedStatus:
+    """
+    Represents the qualified status of a certificate.
+    """
+
+    qualified: bool
+    """
+    Indicates whether the certificate is to be considered qualified.
+    """
+
+    qc_type: QcCertType
+    """
+    Type of qualified certificate.
+    """
+
+    qc_key_security: QcPrivateKeyManagementType
+    """
+    Indicates whether the CA declares that the private key
+    corresponding to this certificate resides in a qualified
+    signature creation device (QSCD) or secure signature creation device (SSCD).
+    It also indicates whether the QCSD is managed on behalf of the signer,
+    if applicable.
+
+    .. warning::
+        These terms are functionally interchangeable, the only difference is
+        that "SSCD" is pre-eIDAS terminology.
+    """
+
+
+UNQUALIFIED = QualifiedStatus(
+    qualified=False,
+    qc_type=QcCertType.QC_ESIGN,
+    qc_key_security=QcPrivateKeyManagementType.UNKNOWN,
+)
+
+
+class QualificationAssessor:
+    def __init__(self, tsp_registry: TSPRegistry):
+        self._registry = tsp_registry
+
+    @staticmethod
+    def _process_qc_statements(cert: x509.Certificate) -> QualifiedStatus:
+        qcs = qualified_asn1.get_qc_statements(cert)
+        qualified = False
+        key_secure = False
+        qc_type = QcCertType.QC_ESIGN
+        for statement in qcs:
+            st_type = statement['statement_id'].native
+            if st_type == 'qc_compliance':
+                qualified = True
+            elif st_type == 'qc_sscd':
+                # management delegation is not encoded by the QcStatements
+                key_secure = True
+            elif st_type == 'qc_type':
+                qc_types: qualified_asn1.QcCertificateType = statement[
+                    'statement_info'
+                ]
+                if len(qc_types) != 1:
+                    # In theory this is not limited to one value, we have to
+                    # let the TL override in a case like this.
+                    # Nonetheless there's really no good reason to do this,
+                    # and some ETSI specs are more strict than others,
+                    # so I'll deal with this case when it presents itself
+                    raise NotImplementedError("only support exactly 1 qc_type")
+                qc_type = QcCertType(qc_types[0].native)
+        return QualifiedStatus(
+            qualified=qualified,
+            qc_type=qc_type,
+            qc_key_security=(
+                QcPrivateKeyManagementType.QCSD
+                if key_secure and qualified
+                else QcPrivateKeyManagementType.UNKNOWN
+            ),
+        )
+
+    @staticmethod
+    def _check_cd_applicable(
+        sd: CAServiceInformation, putative_status: QualifiedStatus
+    ):
+        sd_declared_type = sd.base_info.additional_info_certificate_type
+        if sd_declared_type and putative_status.qc_type not in sd_declared_type:
+            logger.info(
+                f"Found matching SDI {sd.base_info.service_name} on path; "
+                f"skipping because QC type does not match"
+            )
+            return False
+        return True
+
+    @staticmethod
+    def _apply_sd_qualifications(
+        cert: x509.Certificate,
+        prelim_status: QualifiedStatus,
+        sd: CAServiceInformation,
+    ):
+        applicable_qualifiers: Set[Qualifier] = set()
+        for qualification in sd.qualifications:
+            if not qualification.criteria_list.matches(cert):
+                continue
+            applicable_qualifiers.update(qualification.qualifiers)
+        return QualificationAssessor._final_status(
+            prelim_status, frozenset(applicable_qualifiers)
+        )
+
+    @staticmethod
+    def _final_status(
+        prelim_status: QualifiedStatus,
+        applicable_qualifiers: FrozenSet[Qualifier],
+    ):
+        # TODO explicitly check consistency / contradictory qualifiers
+        # (for now we just use conservative defaults)
+        is_qualified: bool
+        if (
+            Qualifier.NOT_QUALIFIED in applicable_qualifiers
+            or Qualifier.LEGAL_PERSON in applicable_qualifiers
+        ):
+            is_qualified = False
+        elif Qualifier.QC_STATEMENT in applicable_qualifiers:
+            is_qualified = True
+        else:
+            is_qualified = prelim_status.qualified
+
+        qc_type: QcCertType
+        if Qualifier.FOR_WSA in applicable_qualifiers:
+            qc_type = QcCertType.QC_WEB
+        elif Qualifier.FOR_ESIG in applicable_qualifiers:
+            qc_type = QcCertType.QC_ESIGN
+        elif Qualifier.FOR_ESEAL in applicable_qualifiers:
+            qc_type = QcCertType.QC_ESEAL
+        else:
+            qc_type = prelim_status.qc_type
+
+        key_mgmt: QcPrivateKeyManagementType
+        if not is_qualified:
+            key_mgmt = QcPrivateKeyManagementType.UNKNOWN
+        elif (
+            Qualifier.NO_SSCD in applicable_qualifiers
+            or Qualifier.NO_QSCD in applicable_qualifiers
+        ):
+            key_mgmt = QcPrivateKeyManagementType.UNKNOWN
+        elif Qualifier.QSCD_MANAGED_ON_BEHALF in applicable_qualifiers:
+            key_mgmt = QcPrivateKeyManagementType.QCSD_DELEGATED
+        elif (
+            Qualifier.WITH_SSCD in applicable_qualifiers
+            or Qualifier.WITH_QSCD in applicable_qualifiers
+        ):
+            key_mgmt = QcPrivateKeyManagementType.QCSD
+        else:
+            key_mgmt = prelim_status.qc_key_security
+        return QualifiedStatus(
+            qualified=is_qualified,
+            qc_type=qc_type,
+            qc_key_security=key_mgmt,
+        )
+
+    def check_entity_cert_qualified(
+        self, path: ValidationPath, moment: Optional[datetime] = None
+    ) -> QualifiedStatus:
+        cert = path.leaf
+        if not isinstance(cert, x509.Certificate):
+            raise NotImplementedError(
+                "Only public-key certs are in scope for qualification"
+            )
+        prelim_status = QualificationAssessor._process_qc_statements(cert)
+        path_policies = path.qualified_policies()
+        reference_time = moment or datetime.now(tz=zoneinfo.ZoneInfo('CET'))
+        if reference_time < EIDAS_START_DATE and path_policies:
+            # check QCP / QCP+ policy
+            policy_oids = {q.user_domain_policy_id for q in path_policies}
+            if PRE_EIDAS_QCP_PLUS_POLICY in policy_oids:
+                prelim_status = replace(
+                    prelim_status,
+                    qualified=True,
+                    qc_key_security=(
+                        QcPrivateKeyManagementType.QCSD_BY_POLICY
+                        if not prelim_status.qc_key_security.is_qcsd
+                        else prelim_status.qc_key_security
+                    ),
+                )
+            elif PRE_EIDAS_QCP_POLICY in policy_oids:
+                prelim_status = replace(prelim_status, qualified=True)
+
+        statuses_found: List[Tuple[CAServiceInformation, QualifiedStatus]] = []
+        for sd in self._registry.applicable_tsps_on_path(path):
+            # For this subtlety, see the hanging para in the beginning of
+            # section 4 in the CEF eSignature DSS validation algorithm doc
+            putative_status = QualificationAssessor._apply_sd_qualifications(
+                cert, prelim_status, sd
+            )
+            if QualificationAssessor._check_cd_applicable(sd, putative_status):
+                statuses_found.append((sd, putative_status))
+
+        uniq_statuses = set(st for _, st in statuses_found)
+        if len(statuses_found) == 1:
+            # happy path
+            return statuses_found[0][1]
+        elif len(uniq_statuses) == 1:
+            # TODO gather these warnings somewhere so they can be added
+            #  to the validation report
+            service_info = ', '.join(
+                sd.base_info.service_name for sd, _ in statuses_found
+            )
+            logger.warning(
+                f"Qualification algorithm for {cert.subject.human_friendly} "
+                f"reached a consistent conclusion, but through several "
+                f"different service definitions: {service_info}"
+            )
+            return statuses_found[0][1]
+        elif not uniq_statuses:
+            return UNQUALIFIED
+        else:
+            service_info = ', '.join(
+                sd.base_info.service_name for sd, _ in statuses_found
+            )
+            logger.warning(
+                f"Qualification algorithm for {cert.subject.human_friendly} "
+                f"reached contradictory conclusions: {uniq_statuses}. "
+                f"Several service definitions were found applicable: "
+                f"{service_info}. This certificate will not be considered "
+                f"qualified."
+            )
+            return UNQUALIFIED
